d03856ebb0c2653238eb9a4a67241078a5b1eacf
Meidika Wardana <meidika@Mac-mini.local>
2015-04-13 08:23:39 +0700

    Initial Commit

---
+++ ToDoList2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList2
+//
+//  Created by Meidika Wardana on 4/13/15.
+//  Copyright (c) 2015 BEI5000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList2
+//
+//  Created by Meidika Wardana on 4/13/15.
+//  Copyright (c) 2015 BEI5000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
